60f2aecfc1ccd1806eff47c0f9db5b9674174b72
kimch-ijjim <dlawldjs02@gmail.com>
2025-08-20 19:09:52 +0900

    Fix: framer-motion import 구문 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// 컴포넌트 import
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import GlobalStyle from "./styles/GlobalStyle";
+
+// 페이지 컴포넌트 import
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import About from "./pages/About";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyle";
-import Project1 from "./pages/Project1/Project1"
+// 1. Project1 -> ProjectDetailPage로 이름 변경 (더 명확한 이름)
+import ProjectDetailPage from "./pages/Project1/Project1"; 
+import NotFound from "./pages/NotFound"; // NotFound 페이지 import 추가
 
 function App() {
   return (
@@ -17,14 +24,21 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/portfolio/project-1" element={<Project1 />} />
-          <Route path="/about" element={<About />} />
+          {/* 2. 동적 라우팅으로 변경 (예: /portfolio/project-1, /portfolio/project-2 등 모두 처리) */}
+          <Route path="/portfolio/:projectId" element={<ProjectDetailPage />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/about" element={<About />} />
+          
+          {/* 3. 중복된 /about 경로 제거 */}
+
+          {/* 4. '페이지 없음(Not Found)'을 위한 경로 추가 */}
+          {/* 위에서 일치하는 경로가 없을 경우, 가장 마지막에 있는 이 라우트가 잡힙니다. */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
     </>
   );
 }
+
 export default App;
